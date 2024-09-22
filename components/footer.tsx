20c9dca9fbddc532030b352f4c36106ceb86043c
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-09-23 02:16:36 +0530

    Update copyright

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <section className="flex flex-col justify-center items-center md:flex-row md:justify-between">
       <div className="text-sm font-normal text-[#AFAFAF] mb-6 md:mb-0">
-        <p>© 2024 Mithril AI Inc.</p>
+        <p>© 2024 Mithril AI, Inc.</p>
       </div>
       <div className="flex flex-wrap space-x-4 text-[10px] md:text-xs font-normal tracking-wider">
         <a
