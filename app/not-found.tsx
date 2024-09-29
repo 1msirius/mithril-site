ec96c3f11238af19b6566ff514298af168666712
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-09-30 00:24:03 +0530

    Update 404 error page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,11 +2,10 @@ import React from "react";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center h-[83vh] text-center px-4">
-      <h1 className="text-5xl mb-8">404</h1>
-      <p className="text-base mb-8 max-w-md leading-[26px]">
-        The page you were looking for doesn&apos;t exist. You may have mistyped the
-        address or the page may have moved.
+    <div className="flex flex-col items-center justify-center h-[83vh] text-center px-4 gap-8">
+      <h1 className="text-5xl">404</h1>
+      <p className="text-lg max-w-md">
+        Oops, the page you were looking for doesn&apos;t exist.
       </p>
       <a href="/">
         <button className="bg-white text-black px-6 py-2 rounded-full text-base font-medium hover:bg-gray-200 transition-colors">
