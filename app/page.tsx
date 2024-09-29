9635d2cfabf4ec0748f374e409a392881d9f8c09
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-09-30 00:24:56 +0530

    Update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-[83vh] text-center px-4">
-      <h1 className="text-3xl md:text-5xl mb-8">
+    <div className="flex flex-col items-center justify-center h-[83vh] text-center px-4 gap-8">
+      <h1 className="text-3xl md:text-5xl">
         Open Science AI Research Lab
       </h1>
-      <p className="text-lg mb-8 max-w-sm jet-brains">
+      <p className="text-lg max-w-sm jet-brains">
         Open models across diverse domains, for everyone, everywhere.
       </p>
       <div className="flex gap-4 items-center">
